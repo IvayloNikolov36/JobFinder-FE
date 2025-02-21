8cce75335f6c90ab9bed7c9bcd3c2d0199790f4d
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-02-21 07:37:37 +0200

    refactoring - removed subscriptions and some signals are used

--- JobFinder/src/app/components/job-advertisements/job-advertisements.component.ts
+++ JobFinder/src/app/components/job-advertisements/job-advertisements.component.ts
@@ -1,46 +1,42 @@
-import { ToastrService } from 'ngx-toastr';
+import { Component, computed, OnInit, signal, Signal, WritableSignal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { SubscriptionsService } from './../../services/subscriptions.service';
 import { JobAdvertisementsService } from '../../services/job-advertisements.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import { BasicModel } from '../../models/basic.model';
-import { JobAd } from '../../models/job-ad';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { BasicModel, JobAd } from '../../models';
 import { NomenclatureService } from '../../core/services';
+import { ToastrService } from 'ngx-toastr';
+
+const ShowFiltersText: string = 'Show Filters';
+const HideFiltersText: string = 'Hide Filters';
+const SortByValues: string[] = ['Published', 'Salary'];
+const InitialPage: number = 1;
+const SelectValueNone: number = 0;
+const MinItemsOnPage: number = 5;
 
 @Component({
   selector: 'jf-job-advertisements',
   templateUrl: './job-advertisements.component.html',
   standalone: false
 })
-export class JobAdvertisementsComponent implements OnInit, OnDestroy {
+export class JobAdvertisementsComponent implements OnInit {
 
-  categories$!: Observable<BasicModel[]>;
-  engagements$!: Observable<BasicModel[]>;
-  jobAds!: JobAd[];
+  categories!: Signal<BasicModel[]>;
+  engagements!: Signal<BasicModel[]>;
+  jobAds: JobAd[] = [];
+
   totalCount!: number;
-
-  subscriptions: Subscription[] = [];
-
-  searchText!: string;
-  currentPage!: number;
-  itemsCount!: number;
-  location!: string;
-  category!: number;
-  engagement!: number;
-  sortBy!: string;
-  isAscending!: boolean;
+  searchText: string = '';
   showFiltersArea: boolean = false;
-  showSubscribeButton: boolean = false;
+  filterButtonLabel: string = ShowFiltersText;
+  currentPage: number = InitialPage;
+  itemsCount: number = MinItemsOnPage;
+  engagement: number = SelectValueNone;
+  sortBy: string = SortByValues[0];
+  isAscending: boolean = true;
 
+  readonly itemsCountArray: number[] = [MinItemsOnPage, 10, 15, 20, 30, 50, 100];
   readonly locationsArray: string[] = ['All', 'Sofia', 'Plovdiv', 'Varna', 'Burgas', 'Ruse', 'Stara Zagora', 'Pleven', 'Veliko Tarnovo'];
-  readonly itemsCountArray: number[] = [5, 10, 15, 20, 30, 50, 100];
-  readonly initialPage = 1;
-  readonly selectValueNone: number = 0;
-  readonly showFiltersText: string = 'Show Filters';
-  readonly hideFiltersText: string = 'Hide Filters';
-
-  buttonText: string = this.showFiltersText;
 
   constructor(
     private route: ActivatedRoute,
@@ -51,22 +47,23 @@ export class JobAdvertisementsComponent implements OnInit, OnDestroy {
     private toastr: ToastrService
   ) {
     this.getDataFromQueryParams();
+    this.getNomenclaturesData();
   }
+
+  location: WritableSignal<string> = signal(this.locationsArray[0]);
+  category: WritableSignal<number> = signal(SelectValueNone);
+
+  showSubscribeButton: Signal<boolean> = computed(() => {
+    return this.location() !== this.locationsArray[0] || this.category() !== 0;
+  });
 
   ngOnInit(): void {
     this.getJobAds();
-    this.categories$ = this.nomenclatureService.getJobCategories();
-    this.engagements$ = this.nomenclatureService.getJobEngagements();
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(s => s.unsubscribe());
-  }
-
-  searchJob(): void {
-    this.currentPage = this.initialPage;
+  searchForJobAds(): void {
+    this.currentPage = InitialPage;
     this.updateQueryParams({ page: this.currentPage, searchText: this.searchText });
-    this.showSubscribeButton = this.showSubscribeForJobs();
     this.getJobAds();
   }
 
@@ -77,8 +74,8 @@ export class JobAdvertisementsComponent implements OnInit, OnDestroy {
   subscribeForJobs(): void {
     this.subscriptionsService
       .subscribeForJobsWithCriterias(
-        this.category === 0 ? null : this.category,
-        this.location === this.locationsArray[0] ? null : this.location)
+        this.category() === 0 ? null : this.category(),
+        this.location() === this.locationsArray[0] ? null : this.location())
       .subscribe({
         next: () => this.toastr.success("Succsessfully subscribed for jobs with selected criterias."),
         error: (err) => this.toastr.error(err.title)
@@ -87,41 +84,41 @@ export class JobAdvertisementsComponent implements OnInit, OnDestroy {
 
   changeItemsCount(selectedItemsCount: number): void {
     this.itemsCount = selectedItemsCount;
-    this.currentPage = this.initialPage;
+    this.currentPage = InitialPage;
     this.updateQueryParams({ page: this.currentPage, items: selectedItemsCount });
     this.getJobAds();
   }
 
   changeFilterLocation(selectedLocation: string): void {
-    this.currentPage = this.initialPage;
-    this.location = selectedLocation;
+    this.currentPage = InitialPage;
+    this.location.set(selectedLocation);
     this.updateQueryParams({ page: this.currentPage, location: selectedLocation });
     this.getJobAds();
   }
 
   changeFilterCategory(selectedCategory: string): void {
-    this.currentPage = this.initialPage;
-    this.category = parseInt(selectedCategory);
+    this.currentPage = InitialPage;
+    this.category.set(parseInt(selectedCategory));
     this.updateQueryParams({ page: this.currentPage, category: selectedCategory });
     this.getJobAds();
   }
 
   changeFilterEngagement(selectedEngagement: number): void {
-    this.currentPage = this.initialPage;
+    this.currentPage = InitialPage;
     this.engagement = selectedEngagement;
     this.updateQueryParams({ page: this.currentPage, engagement: selectedEngagement });
     this.getJobAds();
   }
 
   changeSortBy(sortBy: string): void {
-    this.currentPage = this.initialPage;
+    this.currentPage = InitialPage;
     this.sortBy = sortBy;
     this.updateQueryParams({ page: this.currentPage, sortBy: sortBy });
     this.getJobAds();
   }
 
   changeSortingOrder(orderValue: string): void {
-    this.currentPage = this.initialPage;
+    this.currentPage = InitialPage;
     this.isAscending = orderValue === 'true' ? true : false;
     this.updateQueryParams({ page: this.currentPage, isAscending: orderValue });
     this.getJobAds();
@@ -136,17 +133,11 @@ export class JobAdvertisementsComponent implements OnInit, OnDestroy {
     this.getJobAds();
   }
 
-  // TODO: create it private and use variable in the template
-  showOrHideFilters(): void {
+  toggleFilters(): void {
     this.showFiltersArea = !this.showFiltersArea;
-    this.buttonText === this.showFiltersText
-      ? this.buttonText = this.hideFiltersText
-      : this.buttonText = this.showFiltersText;
-  }
-
-  // TODO: create signal to reevaluate the value of showSubscribeButton based on location and category
-  private showSubscribeForJobs(): boolean {
-    return this.location !== this.locationsArray[0] || this.category !== 0;
+    this.filterButtonLabel === ShowFiltersText
+      ? this.filterButtonLabel = HideFiltersText
+      : this.filterButtonLabel = ShowFiltersText;
   }
 
   private updateQueryParams(queryParamsObject: object): void {
@@ -160,12 +151,12 @@ export class JobAdvertisementsComponent implements OnInit, OnDestroy {
 
   // TODO: create filterModel that contains searchText, engagement...
   private getJobAds(): void {
-    const subscription: Subscription = this.jobAdsService.all(
+    this.jobAdsService.all(
       this.currentPage,
       this.itemsCount,
       this.searchText,
-      this.location,
-      this.category,
+      this.location(),
+      this.category(),
       this.engagement,
       this.sortBy,
       this.isAscending)
@@ -173,30 +164,32 @@ export class JobAdvertisementsComponent implements OnInit, OnDestroy {
         this.totalCount = data.totalCount;
         this.jobAds = data.data;
       });
-
-    this.subscriptions.push(subscription);
   }
 
   private getDataFromQueryParams = (): void => {
     const queryParams: ParamMap = this.route.snapshot.queryParamMap;
 
     const pageValue: string | null = queryParams.get('page');
-    this.currentPage = pageValue === null ? this.initialPage : parseInt(pageValue);
+    this.currentPage = pageValue === null ? InitialPage : parseInt(pageValue);
 
     const itemsValue: string | null = queryParams.get('items');
-    this.itemsCount = itemsValue === null ? this.itemsCountArray[0] : parseInt(itemsValue);
+    this.itemsCount = itemsValue === null ? MinItemsOnPage : parseInt(itemsValue);
 
     this.searchText = queryParams.get('searchText') ?? '';
-    this.location = queryParams.get('location') ?? this.locationsArray[0];
+    this.location.set(queryParams.get('location') ?? this.locationsArray[0]);
 
     const categoryValue: string | null = queryParams.get('category');
-    this.category = categoryValue === null ? this.selectValueNone : parseInt(categoryValue);
+    this.category.set(categoryValue === null ? SelectValueNone : parseInt(categoryValue));
 
     const engagementValue: string | null = queryParams.get('engagement');
-    this.engagement = engagementValue === null ? this.selectValueNone : parseInt(engagementValue);
+    this.engagement = engagementValue === null ? SelectValueNone : parseInt(engagementValue);
 
-    // TODO: create array with values for sort
-    this.sortBy = queryParams.get('sortBy') ?? 'Published';
+    this.sortBy = queryParams.get('sortBy') ?? SortByValues[0];
     this.isAscending = queryParams.get('isAscending') === 'true' ? true : false;
+  }
+
+  private getNomenclaturesData(): void {
+    this.categories = toSignal(this.nomenclatureService.getJobCategories(), { initialValue: [] });
+    this.engagements = toSignal(this.nomenclatureService.getJobEngagements(), { initialValue: [] });
   }
 }
