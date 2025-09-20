f942f790a636f5da2eca895a832b52ba52147456
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-09-20 04:22:01 +0300

    refactoring job ad component and img and company name to navigate to company details

--- JobFinder/src/app/job-ads/components/job-ad/job-ad.component.ts
+++ JobFinder/src/app/job-ads/components/job-ad/job-ad.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { JobAd } from '../../../core/models';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services';
@@ -8,20 +8,36 @@ import { AuthService } from '../../../core/services';
   templateUrl: './job-ad.component.html',
   standalone: false
 })
-export class JobAdComponent {
+export class JobAdComponent implements OnInit {
 
   @Input() ad!: JobAd;
   @Input() isEven!: boolean;
 
+  cardClass: object = {};
+
   constructor(
     private router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.setCardClassStyleObject();
+  }
 
   viewAdDetails(jobAdId: number): void {
     if (this.authService.isCompany()) {
       this.router.navigate(['/ad', jobAdId]);
     } else {
       this.router.navigate(['/job-ad', jobAdId]);
+    }
+  }
+
+  setCardClassStyleObject(): void {
+    this.cardClass = {
+      'me-5': this.isEven,
+      'ms-5': !this.isEven,
+      'white-blue-gradient': this.isEven,
+      'blue-white-gradient': !this.isEven
     }
   }
 }
